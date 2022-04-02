3aeb1fceac5c251f467cd6e47a890f86ee868b7c
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-04-02 17:52:14 +0200

    feat: rename vars

--- DynamicProgramming/Src/PolicyIter.m
+++ DynamicProgramming/Src/PolicyIter.m
@@ -15,8 +15,8 @@ classdef PolicyIter
         tol;        % Tollerance
         nStates;    % Number of states
         nActions;   % Number of actions
-        policy;     % Current policy
-        value;      % Current state value function
+        pi;         % Current policy
+        V;          % Current state value function
     end
 
     methods
@@ -32,30 +32,30 @@ classdef PolicyIter
             % Set the number of actions
             obj.nActions = size(P, 2);
             % Generate a randomic initial policy
-            obj.policy = randi(obj.nActions, obj.nStates, 1);
+            obj.pi = randi(obj.nActions, obj.nStates, 1);
             % Initialize the state value function
-            obj.value = zeros(obj.nStates, 1);
+            obj.V = zeros(obj.nStates, 1);
         end
 
         % Policy Evaluation
         % Given a policy pi, estimate its value function v_pi
         function obj = policyEval(obj)
             % Initialize the state value function
-            obj.value = zeros(obj.nStates, 1);
+            obj.V = zeros(obj.nStates, 1);
             % Iterate the evaluation, until it's reached a fixed point
             while (1)
                 % Store the old values to compute their variations
-                oldValue = obj.value;
+                oldValue = obj.V;
                 % Iterate on states
                 for s = 1 : obj.nStates
                     % Update the estimates
-                    obj.value(s) = obj.R(s, obj.policy(s)) + obj.gamma * ...
-                        squeeze(obj.P(s, obj.policy(s), :))' * obj.value;
+                    obj.V(s) = obj.R(s, obj.pi(s)) + obj.gamma * ...
+                        squeeze(obj.P(s, obj.pi(s), :))' * obj.V;
                 end
                 % Compute the max variation of the value function
                 % Inf-norm: max{i}(|x_i|)
                 % Test: vecnorm([0, -1, 2, -4], Inf)
-                if (vecnorm(obj.value - oldValue, Inf) < obj.tol)
+                if (vecnorm(obj.V - oldValue, Inf) < obj.tol)
                     % If the max variation is less than the tollerance stop!
                     break;
                 end
@@ -73,10 +73,10 @@ classdef PolicyIter
                 for a = 1 : obj.nActions
                     % Compute the estimate
                     Qpi(a) = obj.R(s, a) + obj.gamma * ...
-                        squeeze(obj.P(s, a, :))' * obj.value;
+                        squeeze(obj.P(s, a, :))' * obj.V;
                 end
                 % Take the best action
-                [~, obj.policy(s)] = max(Qpi);
+                [~, obj.pi(s)] = max(Qpi);
             end
         end
 
@@ -85,13 +85,13 @@ classdef PolicyIter
             % Iterate alternating policy evaluation and policy improvment
             % until it's reached a fixed point
             while (1)
-                oldPolicy = obj.policy;
+                oldPolicy = obj.pi;
                 % Policy evaluation: pi -> v_pi
                 obj = obj.policyEval();
                 % Policy improvment: pi -> pi' s.t. v_pi' >= v_pi
                 obj = obj.policyImprov();
                 % Check if the new policy is equal to the previous one
-                if (vecnorm(obj.policy - oldPolicy, Inf) == 0)
+                if (vecnorm(obj.pi - oldPolicy, Inf) == 0)
                     % If the two policirs are equal stop!
                     break;
                 end
--- DynamicProgramming/Src/ValueIter.m
+++ DynamicProgramming/Src/ValueIter.m
@@ -15,8 +15,8 @@ classdef ValueIter
         tol;        % Tollerance
         nStates;    % Number of states
         nActions;   % Number of actions
-        policy;     % Current policy
-        value;      % Current state value function
+        pi;         % Current policy
+        V;          % Current state value function
     end
 
     methods
@@ -32,9 +32,9 @@ classdef ValueIter
             % Set the number of actions
             obj.nActions = size(P, 2);
             % Generate a randomic initial policy
-            obj.policy = randi(obj.nActions, obj.nStates, 1);
+            obj.pi = randi(obj.nActions, obj.nStates, 1);
             % Initialize the state value function
-            obj.value = zeros(obj.nStates, 1);
+            obj.V = zeros(obj.nStates, 1);
         end
 
         % Value Iteration
@@ -42,7 +42,7 @@ classdef ValueIter
             % Iterate until it's reached a fixed point
             while (1)
                 % Store the old values to compute their variations
-                oldValue = obj.value;
+                oldValue = obj.V;
                 % Iterate on states
                 for s = 1 : obj.nStates
                     % Compute the state-action value function
@@ -51,13 +51,13 @@ classdef ValueIter
                     for a = 1 : obj.nActions
                         % Compute the estimate
                         Qpi(a) = obj.R(s, a) + obj.gamma * ...
-                            squeeze(obj.P(s, a, :))' * obj.value;
+                            squeeze(obj.P(s, a, :))' * obj.V;
                     end
                     % Take the best action and its value
-                    [obj.value(s), obj.policy(s)] = max(Qpi);
+                    [obj.V(s), obj.pi(s)] = max(Qpi);
                 end
                 % Compute the max variation of the value function
-                if (vecnorm(obj.value - oldValue, Inf) < obj.tol)
+                if (vecnorm(obj.V - oldValue, Inf) < obj.tol)
                     % If the max variation is less than the tollerance stop!
                     break;
                 end
