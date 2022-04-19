e939ea5778e3a1cebd28562a0431686ce3bdc758
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-04-19 12:17:06 +0200

    feat: improve TD algorithms

--- TemporalDifference/Src/TempDiff.m
+++ TemporalDifference/Src/TempDiff.m
@@ -5,7 +5,7 @@
 %  Email: alessandro.tenaglia@uniroma2.it  %
 % ---------------------------------------- %
 
-% Montecarlo
+% Temporal Difference algorithms
 classdef TempDiff
 
     properties
@@ -50,81 +50,91 @@ classdef TempDiff
             % Iterate on episodes
             for e = 1 : obj.nEpisodes
                 % Generate a randomic initial state
-                sts = obj.env.initStates(randi(numel(obj.env.initStates)));
+                s = obj.env.initStates(randi(numel(obj.env.initStates)));
                 % Choose the initial action using the epsilon-greedy method
-                a = epsGreedy(obj, sts);
+                a = epsGreedy(obj, s);
                 % Generate the episode
-                while (~ismember(sts(end), obj.env.obstStates) && ...
-                        ~ismember(sts(end), obj.env.termStates))
-                    % Move on the grid world
-                    [sp, r] = obj.env.move(sts(end), a);
-                    % Choose the next action using the ...
-                    % epsilon-greedy method
+                while (~ismember(s, obj.env.obstStates) && ...
+                        ~ismember(s, obj.env.termStates))
+                    % Execute a step
+                    [sp, r] = obj.env.step(s, a);
+                    % Choose the next action using the epsilon-greedy
+                    % method
                     ap = epsGreedy(obj, sp);
-                    % Update estimates and policy
-                    Qp = obj.Q(sp, ap);
-                    obj.Q(sts(end), a) = obj.Q(sts(end), a) + obj.alpha * ...
-                        (r + obj.gamma * Qp - obj.Q(sts(end), a));
-                    [obj.V(sts(end)), obj.pi(sts(end))] = max(obj.Q(sts(end), :));
-                    % Set state and action for the next episode
-                    sts = [sts, sp];
+                    % Compute the new estimate based on the next state AND
+                    % the next action
+                    Qest = r + obj.gamma * obj.Q(sp, ap);
+                    % Update the state-action value function
+                    obj.Q(s, a) = obj.Q(s, a) + ...
+                        obj.alpha * (Qest - obj.Q(s, a));
+                    % Update the state value function and the policy
+                    [obj.V(s), obj.pi(s)] = max(obj.Q(s, :));
+                    % Set the state and the action for the next episode
+                    s = sp;
                     a = ap;
                 end
             end
         end
 
-        % Montecarlo Control Exploring start
+        % Expected-SARSA algorithm
         function obj = ESARSA(obj)
             % Iterate on episodes
             for e = 1 : obj.nEpisodes
                 % Generate a randomic initial state
-                sts = obj.env.initStates(randi(numel(obj.env.initStates)));
+                s = obj.env.initStates(randi(numel(obj.env.initStates)));
                 % Generate the episode
-                while (~ismember(sts(end), obj.env.obstStates) && ...
-                        ~ismember(sts(end), obj.env.termStates))
+                while (~ismember(s, obj.env.obstStates) && ...
+                        ~ismember(s, obj.env.termStates))
                     % Choose the action using the epsilon-greedy method
-                    a = epsGreedy(obj, sts(end));
-                    % Move on the grid world
-                    [sp, r] = obj.env.move(sts(end), a);
-                    % Update estimates and policy
-                    probs = obj.eps/obj.env.nActions * ones(obj.env.nActions, 1);
-                    [~, amax] = max(obj.Q(sp, :));
-                    probs(amax) = probs(amax) + 1 - obj.eps;
-                    Qp = sum(obj.Q(sp, :) * probs);
-                    obj.Q(sts(end), a) = obj.Q(sts(end), a) + obj.alpha * ...
-                        (r + obj.gamma * Qp - obj.Q(sts(end), a));
-                    [obj.V(sts(end)), obj.pi(sts(end))] = max(obj.Q(sts(end), :));
-                    % Set state and action fo the enxt episode
-                    sts = [sts, sp];
+                    a = epsGreedy(obj, s);
+                    % Execute a step
+                    [sp, r] = obj.env.step(s, a);
+                    % Compute the probability to choose the next action
+                    probs = obj.eps / obj.env.nActions * ...
+                        ones(obj.env.nActions, 1);
+                    probs(obj.pi(sp)) = probs(obj.pi(sp)) + 1 - obj.eps;
+                    % Compute the new estimate based on the expected reward
+                    % of the next state
+                    Qest = r + obj.gamma * obj.Q(sp, :) * probs;
+                    % Update the state-action value function
+                    obj.Q(s, a) = obj.Q(s, a) + ...
+                        obj.alpha * (Qest - obj.Q(s, a));
+                    % Update the state value function and the policy
+                    [obj.V(s), obj.pi(s)] = max(obj.Q(s, :));
+                    % Set the state for the next episode
+                    s = sp;
                 end
             end
         end
 
-        % Montecarlo Control Epsilon greedy
+        % Q-Learning algorithm
         function obj = Qlearning(obj)
             % Iterate on episodes
             for e = 1 : obj.nEpisodes
                 % Generate a randomic initial state
-                sts = obj.env.initStates(randi(numel(obj.env.initStates)));
+                s = obj.env.initStates(randi(numel(obj.env.initStates)));
                 % Generate the episode
-                while (~ismember(sts(end), obj.env.obstStates) && ...
-                        ~ismember(sts(end), obj.env.termStates))
+                while (~ismember(s, obj.env.obstStates) && ...
+                        ~ismember(s, obj.env.termStates))
                     % Choose the action using the epsilon-greedy method
-                    a = epsGreedy(obj, sts(end));
-                    % Move on the grid world
-                    [sp, r] = obj.env.move(sts(end), a);
-                    % Update estimates and policy
-                    Qp = max(obj.Q(sp, :));
-                    obj.Q(sts(end), a) = obj.Q(sts(end), a) + obj.alpha * ...
-                        (r + obj.gamma * Qp - obj.Q(sts(end), a));
-                    [obj.V(sts(end)), obj.pi(sts(end))] = max(obj.Q(sts(end), :));
-                    % Set state and action fo the enxt episode
-                    sts = [sts, sp];
+                    a = epsGreedy(obj, s);
+                    % Execute a step
+                    [sp, r] = obj.env.step(s, a);
+                    % Compute the new estimate based on the maximum of the
+                    % state-action value function
+                    Qest = r + obj.gamma * max(obj.Q(sp, :));
+                    % Update the state-action value function
+                    obj.Q(s, a) = obj.Q(s, a) + ...
+                        obj.alpha * (Qest - obj.Q(s, a));
+                    % Update the state value function and the policy
+                    [obj.V(s), obj.pi(s)] = max(obj.Q(s, :));
+                    % Set the state for the next episode
+                    s = sp;
                 end
             end
         end
 
-        % Montecarlo Control Epsilon greedy
+        % Double Q-Learning algorithm
         function obj = DQlearning(obj)
             % Initialize estimates
             Q1 = zeros(obj.env.nStates, obj.env.nActions);
@@ -132,28 +142,36 @@ classdef TempDiff
             % Iterate on episodes
             for e = 1 : obj.nEpisodes
                 % Generate a randomic initial state
-                sts = obj.env.initStates(randi(numel(obj.env.initStates)));
+                s = obj.env.initStates(randi(numel(obj.env.initStates)));
                 % Generate the episode
-                while (~ismember(sts(end), obj.env.obstStates) && ...
-                        ~ismember(sts(end), obj.env.termStates))
+                while (~ismember(s, obj.env.obstStates) && ...
+                        ~ismember(s, obj.env.termStates))
                     % Choose the action using the epsilon-greedy method
-                    a = epsGreedy(obj, sts(end));
-                    % Move on the grid world
-                    [sp, r] = obj.env.move(sts(end), a);
-                    % Choose the action using the epsilon-greedy
+                    a = epsGreedy(obj, s);
+                    % Execute a step
+                    [sp, r] = obj.env.step(s, a);
+                    % Choose the Q to update randomly
                     if (rand() < 0.5)
-                        Qp = max(Q2(sp, :));
-                        Q1(sts(end), a) = Q1(sts(end), a) + obj.alpha * ...
-                            (r + obj.gamma * Qp - Q1(sts(end), a));
+                        % Compute the new estimate based on the maximum of
+                        % Q2
+                        Qest = r + obj.gamma * max(Q2(sp, :));
+                        % Update Q1
+                        Q1(s, a) = Q1(s, a) + ...
+                            obj.alpha * (Qest - Q1(s, a));
                     else
-                        Qp = max(Q1(sp, :));
-                        Q2(sts(end), a) = Q2(sts(end), a) + obj.alpha * ...
-                            (r + obj.gamma * Qp - Q2(sts(end), a));
+                        % Compute the new estimate based on the maximum of
+                        % Q1
+                        Qest = r + obj.gamma * max(Q1(sp, :));
+                        % Update Q2
+                        Q2(s, a) = Q2(s, a) + ...
+                            obj.alpha * (Qest - Q2(s, a));
                     end
-                    obj.Q(sts(end), a) = 0.5 * (Q1(sts(end), a) + Q2(sts(end), a));
-                    [obj.V(sts(end)), obj.pi(sts(end))] = max(obj.Q(sts(end), :));
-                    % Set state and action fo the enxt episode
-                    sts = [sts, sp];
+                    % Update Q using the average of Q1 and Q2
+                    obj.Q(s, a) = 0.5 * (Q1(s, a) + Q2(s, a));
+                    % Update the state value function and the policy
+                    [obj.V(s), obj.pi(s)] = max(obj.Q(s, :));
+                    % Set the state for the next episode
+                    s = sp;
                 end
             end
         end
