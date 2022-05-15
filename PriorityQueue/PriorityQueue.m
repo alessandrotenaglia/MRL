1ec16fc0b908c2c69567d10d37fc5e6c42d55c65
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-05-15 18:55:32 +0200

    feat: improve priority queue class

--- PriorityQueue/PriorityQueue.m
+++ PriorityQueue/PriorityQueue.m
@@ -1,18 +1,42 @@
-% ---------------------------------------- %
-%  File: PriorityQueue.m                   %
-%  Date: May 12, 2022                      %
-%  Author: Alessandro Tenaglia             %
-%  Email: alessandro.tenaglia@uniroma2.it  %
-% ---------------------------------------- %
+% +----------------------------------------+
+% | File: PriorityQueue.m                  |
+% | Date: May 10, 2022                     |
+% | Author: Alessandro Tenaglia            |
+% | Email: alessandro.tenaglia@uniroma2.it |
+% +--------------------------------------- +
+%
+% PriorityQueue - a simple priority queue with methods of push, pop and
+% contains.
+%
+% Example:
+% dim = 1; PQ = PriorityQueue(dim);
+% n = 5; prios = randperm(n); elems = 1:n;
+% for i = 1 : n
+%     fprintf("Pushing elem: %d with prio: %d\n", elems(i), prios(i));
+%     PQ = PQ.push(prios(i), elems(i));
+%     disp(PQ.elems);
+% end
+% elem = 1;
+% fprintf("Checking if the elem: %d is in the queue... ", elem);
+% [tf, index] = PQ.contains(elem);
+% if (tf)
+%     fprintf("yes at position: %d\n\n", index);
+% else
+%     fptinf("no\n\n")
+% end
+% elem = 7;
+% fprintf("Checking if the elem: %d is in the queue... ", elem);
+% [tf, index] = PQ.contains(elem);
+% if (tf)
+%     fprintf("yes at position: %d\n\n", index);
+% else
+%     fprintf("nope\n\n");
+% end
+% fprintf("Popping the first elem: %d\n", PQ.elems(1, 2));
+% [PQ, elem] = PQ.pop();
+% disp(PQ.elems);
 
-% Priority Queue
 classdef PriorityQueue
-    % PriorityQueue
-    %
-    % Example:
-    % dim = 1; PQ = PriorityQueue(dim)
-    % for i = 1 : 7; PQ = PQ.push(randi(10, dim, 1), i); end; PQ.elems
-    % [PQ, elem] = PQ.pop(); elem, PQ.elems
 
     properties
         dim;    % Dimension of elements
@@ -21,21 +45,35 @@ classdef PriorityQueue
     end
 
     methods
-        % Class constructor
+        % Priority Queue class constructor.
         function obj = PriorityQueue(dim)
-            % Set properties
-            obj.dim = dim;
+            % Check number of input arguments
+            if (nargin == 0)
+                % Set the default dimension
+                obj.dim = 1;
+            else
+                % Set the dimension
+                obj.dim = dim;
+            end
             % Initialize queue
-            obj.nElems = 0;
-            obj.elems = [];
+            obj = obj.clear();
         end
 
-        % Push a new element into the queue
-        function obj = push(obj, prio, elem)
+        % Pushes a new element into the queue with a specific priority.
+        %
+        % param prio: the priority to assign to the new element;
+        % param elem: the element to push into the queue;
+        %
+        % return index: the index where the element is added;
+        function [obj, index] = push(obj, prio, elem)
             % Check if the queue contains already the element
             [tf, index] = obj.contains(elem);
             if (tf)
-                % Decrease the counter of elements
+                % Check if the element has already the desired priority
+                if (obj.elems(index, 1) == prio)
+                    return;
+                end
+                % Decrement the counter of elements
                 obj.nElems = obj.nElems - 1;
                 % Remove the element from the queue
                 obj.elems = [obj.elems(1:index-1, :); ...
@@ -62,8 +100,14 @@ classdef PriorityQueue
             end
         end
 
-        % Pop the first element of the queue
+        % Pops the first element of the queue.
+        %
+        % return elem: the element that has been removed
         function [obj, elem] = pop(obj)
+            % Check the element dimension
+            if (obj.nElems == 0)
+                error('Error: the queue is empty.')
+            end
             % Save the first element
             elem = obj.elems(1, 2:end);
             % Decrease the counter of elements
@@ -72,12 +116,16 @@ classdef PriorityQueue
             obj.elems(1, :) = [];
         end
 
-        % Check if the queue contains a specific element
+        % Checks if the queue contains a specific element.
+        %
+        % param elem: the element to search;
+        %
+        % return tf: true/false if the element is in the queue
+        % return index: the index where the element is placed;
         function [tf, index] = contains(obj, elem)
-            % Check the element size
-            if (size(elem, 1) ~= 1 || size(elem, 2) ~= obj.dim)
-                disp('Error: invalid element size')
-                return
+            % Check the element dimension
+            if (~all(size(elem) == [1, obj.dim]))
+                error('Error: the element dimension is not valid.')
             end
             % Check if the queue is empty
             if obj.nElems == 0
@@ -88,11 +136,11 @@ classdef PriorityQueue
             end
         end
 
-        % Clear the queue
+        % Clears the queue.
         function obj = clear(obj)
-            % Reset the counter
+            % Reset the counter of elements
             obj.nElems = 0;
-            % Clear the queue
+            % Clear all the elements
             obj.elems = [];
         end
     end
